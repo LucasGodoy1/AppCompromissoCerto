e147bf9f315d0a92d589b8120664bc339572e70a
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-11-27 21:35:34 -0300

    feat: Tela editar e seus componenetes

---
+++ app/src/main/java/lucasgodoy1/com/github/compromissocerto/controller/EditarActivityController.kt
@@ -0,0 +1,62 @@
+package lucasgodoy1.com.github.compromissocerto.controller
+
+import android.content.ContentValues
+import android.content.Context
+import android.widget.Toast
+import lucasgodoy1.com.github.compromissocerto.data.CompromissoDataBase
+import lucasgodoy1.com.github.compromissocerto.ui.EditarActivity
+import lucasgodoy1.com.github.compromissocerto.util.formatarData
+import lucasgodoy1.com.github.compromissocerto.util.validarCampo
+import lucasgodoy1.com.github.compromissocerto.view.EditarActivityComponente
+
+class EditarActivityController(val editarActivity: EditarActivity) {
+    val editarActivityComponente = EditarActivityComponente(editarActivity)
+    val preferences = editarActivity.getSharedPreferences("EDITAR_TEMP", Context.MODE_PRIVATE)
+
+    val compromissoDataBase = CompromissoDataBase(editarActivity)
+
+    var contentValues = ContentValues()
+
+    val id = preferences.getString("ID", "") ?: ""
+    val compromisso = preferences.getString("COMPROMISSO", "") ?: ""
+    val data = preferences.getString("DATA", "") ?: ""
+    val hora = preferences.getString("HORA", "") ?: ""
+
+
+    fun seuCompromissoAtualEdit() {
+        editarActivityComponente.editCompromisso.setText(compromisso)
+        editarActivityComponente.editHora.setText(hora)
+    }
+
+
+    fun botaoSalvarTelaEditar() {
+
+        editarActivityComponente.btnSalvar.setOnClickListener {
+            if (validarCampo(editarActivityComponente.editCompromisso) && validarCampo(editarActivityComponente.editHora)) {
+
+                contentValues.put(
+                    "COMPROMISSO",
+                    editarActivityComponente.editCompromisso.text.toString()
+                )
+                contentValues.put("DATA", formatarData(editarActivityComponente.editCalendario.date))
+                contentValues.put("HORA", editarActivityComponente.editHora.text.toString())
+
+                if (id.isNotEmpty()) {
+                    val rowsUpdated = compromissoDataBase.atualizarDados(id.toInt(), contentValues)
+                    if (rowsUpdated > 0) {
+                        Toast.makeText(
+                            editarActivity,
+                            "Suas Alterações foram Salvas com Sucesso",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                preferences.edit().clear().apply()
+                editarActivity.finish()
+            }
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/lucasgodoy1/com/github/compromissocerto/view/EditarActivityComponente.kt
@@ -0,0 +1,17 @@
+package lucasgodoy1.com.github.compromissocerto.view
+
+import android.view.View
+import android.widget.CalendarView
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
+import lucasgodoy1.com.github.compromissocerto.R
+import lucasgodoy1.com.github.compromissocerto.ui.EditarActivity
+
+class EditarActivityComponente (val editarActivity : EditarActivity){
+
+    var editCompromisso : EditText = editarActivity.findViewById(R.id.idEditandoCompromisso)
+    var editCalendario : CalendarView = editarActivity.findViewById(R.id.idCalendario)
+    var editHora : EditText = editarActivity.findViewById(R.id.idEditTime)
+    val btnSalvar : AppCompatButton = editarActivity.findViewById(R.id.idEditBtnSalvar)
+
+}
